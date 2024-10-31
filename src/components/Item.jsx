fc70535ca181ee2c9aa47e0425bdfd8e301a9f56
Jessen <jessen123ptk@gmail.com>
2024-10-31 12:36:21 +0700

    Initial commit

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+export default function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li key={item.id}>
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={{ textDecoration: item.checked ? "line-through" : "none" }}>
+        {item.quantity} {item.name}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
+    </li>
+  );
+}
